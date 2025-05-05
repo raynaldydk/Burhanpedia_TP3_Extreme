10471ee5b031926cee8507e3b1819195afc05ed9
Raynaldy Dwi <raynaldydk10@gmail.com>
2025-05-05 07:52:10 +0700

    add menu penjual

--- src/system/SystemPenjual.java
+++ src/system/SystemPenjual.java
@@ -18,12 +18,62 @@ public class SystemPenjual implements SystemMenu{
 
     @Override
     public String showMenu() {
-        return "";
+        return """
+                
+                ===== MENU PENJUAL =====
+                1. Cek Produk
+                2. Tambah Produk
+                3. Tambah Stok
+                4. Ubah Harga Barang
+                5. Kirim Barang
+                6. Lihat Laporan Pendapatan
+                7. Cek Saldo
+                8. Lihat Riwayat Transaksi
+                9. Kembali ke Menu Utama
+                """;
     }
 
     @Override
     public void handleMenu() {
+        String pilihan;
 
+        do{
+            System.out.println(showMenu());
+            System.out.print("Perintah: ");
+            pilihan = input.nextLine();
+
+            switch (pilihan){
+                case "1":
+                    handleCekProduk();
+                    break;
+                case "2":
+                    handleTambahProduk();
+                    break;
+                case "3":
+                    handleTambahStok();
+                    break;
+                case "4":
+                    handleUbahHarga();
+                    break;
+                case "5":
+                    handleKirimBarang();
+                    break;
+                case "6":
+                    handleLaporanPendapatan();
+                    break;
+                case "7":
+                    handleCekSaldo();
+                    break;
+                case "8":
+                    handleRiwayatTransaksi();
+                    break;
+                case "9":
+                    break;
+                default:
+                    System.out.println("Input salah!\n");
+                    break;
+            }
+        }while(!pilihan.equals("9"));
     }
 
     public void handleCekProduk(){
